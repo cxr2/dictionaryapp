c144aa5d7b318f170d383fbbb1898ff791dba541
cxr2 <ciosahoulihan@gmail.com>
2021-04-07 09:42:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary</h1>
+      </header>
+      <h2>
+        created by <a href="https://cjhoulihan.com">C.J.Houlihan</a>
+      </h2>
+    </div>
+  );
+}
+
+export default App;
